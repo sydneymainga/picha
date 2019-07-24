1ca4e2075620787ff5df5d248fe6cffe1cce4efe
Brian Kimathi <kimbrian400@gmail.com>
2019-07-24 13:16:55 +0300

    Comm_leader & health_worker xml

---
+++ app/src/main/java/com/example/picha/Health_worker.java
@@ -0,0 +1,14 @@
+package com.example.picha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Health_worker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_worker);
+    }
+}
